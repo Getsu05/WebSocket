77c8c30dd2063f86e175593225462cb724f0d93a
Getsu05 <scortacansmartin@gmial.com>
2025-05-18 21:37:38 +0200

    Agrega nuevas funciones / escenas / cambios en WebSocket

---
+++ servidor-websocket/server.js
@@ -0,0 +1,21 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log("Nuevo cliente conectado");
+
+  ws.on('message', (data) => {
+    // Enviar a todos los clientes conectados
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log("Cliente desconectado");
+  });
+});
+
+console.log("Servidor WebSocket en ws://localhost:8080");
